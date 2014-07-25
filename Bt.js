c26ab0a1d798c210d5bc8f69bb53ee41a9869ffd
Daegwang Jang <flashnode@gmail.com>
2014-07-25 23:50:42 +0900

    integrate
    integrate

--- Bt.js
+++ Bt.js
@@ -17,6 +17,24 @@ Bt.extend = function(obj, prop) {
 	return obj;
 };
 
+(function(){
+	var agent = navigator.userAgent.toLowerCase(),
+		isIE = agent.indexOf("msie") > -1;
+	
+	if(msIE){
+		XMLHttpRequest = function(){
+			return new ActiveXObject(
+				navigator.userAgent.indexOf("MSIE 5") >= 0 ?
+				"Microsoft.XMLHTTP" : "Msxml2.XMLHTTP"
+			);
+		}
+	}
+	
+	Bt.extend({
+		isIE: isIE
+	});
+})
+
 Bt.extend(Bt, {
 	color: {
 		blue: "primary",
@@ -37,75 +55,118 @@ Bt.extend(Bt, {
 		dom = document.createElement(name);
 		if(cls) dom.className = cls;
 		return dom;
-	}
-});
-
-Bt.Dom = {
+	},
 	get: function(id){
 		return document.getElementById(id);
 	},
+	objEncode : function(){
+        if(!o){
+            return "";
+        }
+        var buf = [];
+        for(var key in o){
+            var ov = o[key], k = encodeURIComponent(key);
+            var type = typeof ov;
+            if(type == 'undefined'){
+                buf.push(k, "=&");
+            }else if(type != "function" && type != "object"){
+                buf.push(k, "=", encodeURIComponent(ov), "&");
+            }else if(ov instanceof Array){
+                if (ov.length) {
+                    for(var i = 0, len = ov.length; i < len; i++) {
+                        buf.push(k, "=", encodeURIComponent(ov[i] === undefined ? '' : ov[i]), "&");
+                    }
+                } else {
+                    buf.push(k, "=&");
+                }
+            }
+        }
+        buf.pop();
+        return buf.join("");
+	},
+	arrEncode: function(a) {
+		var s = [];
+		
+		if ( a.constructor == Array ) {
+			for ( var i = 0; i < a.length; i++ )
+				s.push( a[i].name + "=" + encodeURIComponent( a[i].value ) );
+			
+		} else {
+			for ( var j in a )
+				s.push( j + "=" + encodeURIComponent( a[j] ) );
+		}
+		
+		return s.join("&");
+	}
+});
+
+Bt.Request = {
+	httpSuccess: function(r) {
+		try {
+			return !r.status && location.protocol == "file:" ||
+					( r.status >= 200 && r.status < 300 );
+		} catch(e) {}
+		return false;
+	},
+	ajax: function(type, url, data){
+		if(!url){
+			var success = type.success;
+			data = type.data;
+			url = typelurl;
+			type = type.type;
+		}
+		
+		var xml = new XMLHttpRequest();
+		xml.open(type || "GET", url, true);
+		var onreadystatechange = function(){
+			if (xml && (xml.readystate == 4)){
+				var status = Bt.Request.httpSuccess(xml) ? "success" : "error";
+				if(status != "error") success(xml, status);
+				xml.onreadystatechange = function(){};
+				xml = null;
+			}
+		};
+		xml.onreadystatechange = onreadystatechange;
+		xml.send(data);
+	}
+}
+
+Bt.Dom = {
+	init: function(){
+		this.getEl();
+		this.setItem();
+	},
 	show: function(id){
 		var dom;
-		if(Bt.Dom.get(id) === null){
+		if(Bt.get(id) === null){
 			dom = document.getElementsByTagName("body").item(); 
 		}
 		else{
 			dom = Bt.Dom.get(id);
 		}
 		dom.appendChild(this.el); 
-	}
-};
-
-
-// Panel
-Bt.Panel = function(config){
-	Bt.extend(this, config);
-	this.init();
-};
-Bt.extend(Bt.Panel.prototype, Bt.Dom);
-Bt.extend(Bt.Panel.prototype, {
-	init: function(){
-		// get DOM Elements
-		this.getEl();
+	},
+	visible: function(v){
+		if(this.el) this.el.style.display = (v ? "block" : "none");
+	},
+	addClass: function(name){
+		this.el.className += (" " + name);
+	},
+	removeClass: function(name){
 		
 	},
-	getEl: function(){
-		var p, pColor, pIcon, pHead, pBody;
-		var color = ((!this.color) ? "default" : Bt.color[this.color]);
-		
-		p = Bt.tag("div");
-		pColor = "panel-" + color;
-		p.className = "panel " + pColor;
-		
-		if(this.title || this.tbar){
-			pHead = Bt.tag("div","panel-heading");
-			if(this.title){
-				pTitle = Bt.tag("h3","panel-title");
-				pTitle.innerText = this.title;
-				pHead.appendChild(pTitle);
+	setItem: function(item){
+		if(this.item instanceof Array){
+			for(var i=0; i<this.item.length; i++){
+				this.mainEl.appendChild(this.item[i].el);
 			}
-			if(this.tbar && this.tbar instanceof Array){
-				for(var i in this.tbar){
-					pHead.appendChild(this.tbar[i].el);
-				}
-			}
-			
 		}
-		pBody = Bt.tag("div","panel-body");
-		
-		p.appendChild(pHead);
-		p.appendChild(pBody);
-		
-		this.el = p;
-		
-		return p;
-		
 	}
-});
+};
 
 
+//Button
 
-// Buttons
 Bt.Button = function(config){
 	Bt.extend(this, config);
 	this.init();
@@ -155,63 +216,286 @@ Bt.extend(Bt.Button.prototype, {
 });
 
 
-/*
- * 
- *  Button Test
- * 
- */
+Bt.Group = function(config){
+	Bt.extend(this, config);
+	this.init();
+};
+Bt.extend(Bt.Group.prototype, Bt.Dom);
+Bt.extend(Bt.Group.prototype, {
+	getEl: function(){
+		var div, btnColor;
+		var size = ((!this.size) ? '' : "btn-group-" + Bt.size[this.size]);
+		div = Bt.tag("div", "btn-group" 
+				+ (this.type ? '-' + this.type : ' ') + size);
+		
+		this.mainEl = div;
+		this.el = div;
+		return div;
+	}
+});
 
-window.onload = function(){
-	aBtn = new Bt.Button({
-		/*
-		 * Button Text
-		 * ================
-		 * default: button
-		 */
-		text: "Text",
-		
-		/*
-		 * Button Color
-		 * ================
-		 * blue-primary
-		 * green-success
-		 * skyblue-info
-		 * orange-warning
-		 * red-danger
-		 * trans-link
-		 */
-		color: "red", 
-	
-		/*
-		 * Button Size
-		 * ===============
-		 * large, default, small, xsmall, block
-		 */
-		size: "xsmall",
-		
-		/*
-		 * Button Icon
-		 * ===============
-		 * Includes 200 glyphs in font format from the Glyphicon Halflings set
-		 * http://glyphicons.com/
-		 */
-		icon: "ok",
-		
-		/*
-		 * Add Event 
-		 */
-		onclick: function(){
-			aBtn.setText("clicked");
+
+//Column
+
+Bt.Col = function(config){
+	Bt.extend(this, config);
+	this.init();
+};
+Bt.extend(Bt.Col.prototype, Bt.Dom);
+Bt.extend(Bt.Col.prototype, {
+	init: function(){
+		this.getEl();
+	},
+	getEl: function(){
+		var div, col;
+		div = Bt.tag('div', 'row');
+		for(var i=0; i<this.item.length; i++){
+			col = Bt.tag('div','col-xs-' + this.item[i].size);
+			col.innerHTML = this.item[i].html;
+			div.appendChild(col);
 		}
-	});
-	
-	aPanel = new Bt.Panel({
-		//title: "panel title",
-		tbar: [aBtn],
-		color: "orange"
-	});
-	aPanel.show();
-	
+		this.el = div;
+		return div;
+	}
+});
 
 
-}
+//default component
+
+Bt.Comp = function(config){
+	Bt.extend(this, config);
+	this.init();
+};
+Bt.extend(Bt.Comp.prototype, Bt.Dom);
+Bt.extend(Bt.Comp.prototype, {
+	init: function(){
+		this.getEl();
+	},
+	getEl: function(){
+		this.el = comp;
+		return comp;
+		
+	}
+});
+
+
+
+//Form
+
+//Input
+Bt.fmInput = function(config){
+	Bt.extend(this, config);
+	this.init();
+};
+Bt.extend(Bt.fmInput.prototype, Bt.Dom);
+Bt.extend(Bt.fmInput.prototype, {
+	init: function(){
+		this.getEl();
+	},
+	getEl: function(){
+		var input;
+		var size = ((!this.size) ? '' : "input-" + Bt.size[this.size]);
+		
+		input = Bt.tag("input", "form-control");
+		if(this.placeholder) input.setAttribute("placeholder",this.placeholder);
+		input.className += " " + size;
+		
+		this.el = input;
+		return input;
+	}
+});
+
+//Select
+Bt.fmSelect = function(config){
+	Bt.extend(this, config);
+	this.init();
+};
+Bt.extend(Bt.fmSelect.prototype, Bt.Dom);
+Bt.extend(Bt.fmSelect.prototype, {
+	init: function(){
+		this.getEl();
+	},
+	getEl: function(){
+		var select, option;
+		select = Bt.tag('select', 'form-control');
+		for(var i=0; i<this.data.length; i++){
+			option = Bt.tag('option');
+			option.innerText = this.data[i];
+			option.value = this.data[i];
+			select.appendChild(option);
+		}
+		return select;
+	}
+});
+
+
+
+//Grid
+
+Bt.Grid = function(config){
+	Bt.extend(this, config);
+	this.init();
+};
+Bt.extend(Bt.Grid.prototype, Bt.Dom);
+Bt.extend(Bt.Grid.prototype, {
+	setStore: function(json, name){
+		var data, record;
+		record = [];
+		data = json[this.name];
+		
+		for(var s in json[name][0]){
+			record.push(s);
+		}
+		
+		this.data = data;
+		this.record = this.activeRecord = record;
+	},
+	load: function(param, callback){
+		var that = this;
+		Bt.Request.ajax({
+			type: this.type,
+			url: this.url + (params ? "&" + Bt.objEncode(params) : ' '),
+			success: function(r, status){
+				var json = JSON.parse(r.responseText);
+				that.setStore(json, that.name);
+				if(that.el){
+					that.el.remove();
+				}
+				that.getEl();
+			}
+		});
+	},
+	getEl: function(){
+		var tb, tr, th, thd, tbd, div;
+		div = Bt.tag('div');
+		div.id = this.id;
+		tb = Bt.tag('table', 'table table-striped');
+		thd = Bt.tag('thead');
+		tr = Bt.tag('tr');
+		th = Bt.tag('th');
+		th.innerText = "#";
+		tr.appendChild(th);
+		
+		for(var i=0; i<this.activeRecord.length; i++){
+			th = Bt.tag('th');
+			th.innerText - this.activeRecord[i];
+			tr.appendChild(th);
+		}
+		thd.appendChild(tr);
+		
+		tbd = Bt.tag('tbody');
+		for(var j=0; j<this.data.length; j++){
+			tr = Bt.tag('tr');
+			td = Bt.tag('td');
+			td.innerText = j + 1;
+			tr.appendChild(td);
+			for(var k=0; k<this.activeRecord.length; k++){
+				td = Bt.tag('td');
+				td.innerText = this.data[j][this.activeRecord[k]];
+				tr.appendChild(td);
+			}
+			tbd.appendChild(tr);
+		}
+		tb.appendChild(thd);
+		tb.appendChild(tbd);
+		div.appendChild(tb);
+		
+		this.el = div;
+		return div;
+	}
+});
+
+
+
+
+//Panel
+
+Bt.Panel = function(config){
+	Bt.extend(this, config);
+	this.init();
+};
+Bt.extend(Bt.Panel.prototype, Bt.Dom);
+Bt.extend(Bt.Panel.prototype, {
+	init: function(){
+		// get DOM Elements
+		this.getEl();
+		
+	},
+	getEl: function(){
+		var p, pColor, pIcon, pHead, pBody;
+		var color = ((!this.color) ? "default" : Bt.color[this.color]);
+		
+		p = Bt.tag("div");
+		pColor = "panel-" + color;
+		p.className = "panel " + pColor;
+		
+		if(this.title || this.tbar){
+			pHead = Bt.tag("div","panel-heading");
+			if(this.title){
+				pTitle = Bt.tag("h3","panel-title");
+				pTitle.innerText = this.title;
+				pHead.appendChild(pTitle);
+			}
+			if(this.tbar && this.tbar instanceof Array){
+				for(var i in this.tbar){
+					pHead.appendChild(this.tbar[i].el);
+				}
+			}
+			p.appendChild(pHead);
+		}
+
+		pBody = Bt.tag("div","panel-body");
+		p.appendChild(pBody);
+		
+		this.mainEl = pBody;
+		this.el = p;
+		
+		return p;
+	}
+});
+
+
+
+//Window
+
+Bt.Window = function(config){
+	Bt.extend(this, config);
+	this.init();
+};
+Bt.extend(Bt.Window.prototype, Bt.Dom);
+Bt.extend(Bt.Window.prototype, {
+	init: function(){
+		this.getEl();
+	},
+	getEl: function(){
+		var m, mDialog, mContent, mHeader, mBody, mFooter, mClose;
+		var $this = this;
+		m = Bt.tag('div', 'modal fade in');
+		m.style.display = "block";
+		mDialog = Bt.tag('div', 'modal-dialog modal-sm');
+		mContent = Bt.tag('div', 'modal-content');
+		mHeader = Bt.tag('div', 'modal-header');
+		mBody = Bt.tag('div', 'modal-body');
+		mFooter = Bt.tag('div', 'modal-footer');
+		
+		if(this.title){
+			mClose = Bt.tag('button', 'close');
+			mClose.innerText = "x";
+			mClose.onclick = function(){
+				$this.visible(false);
+			}
+			
+			mHeader.appendChild(mClose);
+			mHeader.innerHTML += this.title;
+			mContent.appendChild(mHeader);
+		}
+		mContent.appendChild(mBody);
+		mContent.appendChild(mFooter);
+		mDialog.appendChild(mContent);
+		m.appendChild(mDialog);
+		
+		this.el = m;
+		return m;	
+	}
+});
+
